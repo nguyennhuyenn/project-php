6837b4fe1ee7a8ece5adab44612cc3f22e398698
nhuyen1234 <yennnph34437@fpt.edu.vn>
2024-01-08 00:23:03 +0700

    Du An 1

---
+++ Layout/footer.php
@@ -0,0 +1,73 @@
+
+<?php
+// include('./database/config.php');
+?>
+<!--------------------FOOTER--------------------------- -->
+<footer class="section-p1"><!--mục footer -->
+    <div class="col">
+        <h4>HỆ THỐNG CỬA HÀNG</h4><!--Hệ thông cửa hàng -->
+        <p>Quận 1 - 160 Nguyễn Cư Trinh, Phường Nguyễn Cư Trinh.</p>
+        <p>Quận 1 - The New Playground 26 Lý Tự Trọng, Phường Bến Nghé.</p>
+        <p>Quận 1 - Central Market 4 Phạm Ngũ Lão, Phường Phạm Ngũ Lão.</p>
+        <p>Quận 10 - 561 Sư Vạn Hạnh, Phường 13.</p>
+    </div> 
+    <div class="col">
+        <h4>THÔNG TIN LIÊN HỆ</h4><!--Thông tin liên hệ -->
+        <p>Tuyển dụng:<a href ="https://www.facebook.com/DirtyCoins.VN/"> https://www.facebook.com/DirtyCoins.VN/</a> </p>
+        <p>Website:<a href="https://dirtycoins.vn/"> https://dirtycoins.vn/</a></p>
+        <p>Liên hệ CSKH: support@<a href ="https://dirtycoins.vn/lien-he"> https://dirtycoins.vn/lien-he</a></p>
+        <p>For business: contact@<a href ="0933 800 190 - 1900252557"> 0933 800 190 - 1900252557</a></p>
+    </div>
+    <div class="col">
+        <h4>FOLLOW US ON SOCIAL MEDIA</h4><!--Follow us on social media-->
+        <li><i class="fa fa-facebook"></i></li>
+        <li><i class="fa fa-instagram"></i></li>
+        <li><i class="fa fa-youtube"></i></li>            
+    </div>    
+</footer>
+<style>
+/*----------------FOOTER--------------------*/
+
+footer{
+    background:rgb(0, 0, 0);
+    display:flex;
+    margin-top:0px;
+    justify-content: space-around;
+    margin-bottom:0px;
+    padding-bottom: 20px;   /*chỉnh size background đen */
+    padding-left:150px;
+    
+}
+footer.col{
+    
+    display:flex;
+    flex-direction:column;
+    align-items: flex-start;
+    margin-top: 100p;
+}
+footer h4{   /*chỉnh size chữ H4*/
+    color:rgb(255, 255, 255);
+    font-size: 16px;
+    padding-bottom:30px;
+    margin-top:40px;
+    font-weight: bold;
+}
+footer p{  /*chỉnh size chữ P*/
+    color:rgb(255, 255, 255);
+    font-size: 13px;
+    text-decoration:none;
+    margin-bottom:15px;
+
+ 
+}
+footer li{ /*chỉnh icon fb,instagram,youtube*/
+    color:#fff;
+    margin-top: 3px;
+    font-weight: bold;
+    
+   
+}
+
+</style>
+
+<style>
